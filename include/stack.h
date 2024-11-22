d70f85e13770b09971668680bacaa116d8b72136
uzema <uzema_2k16@mail.ru>
2024-11-22 12:01:50 +0300

    wip

--- include/stack.h
+++ include/stack.h
@@ -2,10 +2,11 @@
 #define __STACK_H__
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-template <class T, template <class> class Tcont>
+template <class T, template<class, class...> class Tcont>
 class Stack {
 
 	Tcont<T> data;
@@ -19,4 +20,34 @@ public:
 	size_t size();
 };
 
+template <class T, template<class, class...> class Tcont>
+inline void Stack<T, Tcont>::push(const T& val) {
+	data.push_back(val);
+}
+
+template <class T, template<class, class...> class Tcont>
+inline void Stack<T, Tcont>::pop() {
+	if (data.empty()) {
+		throw "Empty stack";
+	}
+	data.pop_back();
+}
+
+template <class T, template<class, class...> class Tcont>
+inline T& Stack<T, Tcont>::top() {
+	if (data.empty()) {
+		throw "Empty stack, no element on top";
+	}
+	return data.back();
+}
+
+template <class T, template<class, class...> class Tcont>
+inline bool Stack<T, Tcont>::empty() {
+	return (data.size() == 0);
+}
+
+template <class T, template<class, class...> class Tcont>
+inline size_t Stack<T, Tcont>::size() {
+	return data.size();
+}
 #endif
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,3 +1,44 @@
 #include "stack.h"
-
 #include <gtest.h>
+#include <vector>
+
+TEST(stack, EmptyStackSizeIs0) {
+	Stack<int, std::vector> intStack;
+	ASSERT_EQ(intStack.size(), 0);
+}
+
+TEST(stack, EmptyStackIsEmpty) {
+	Stack<int, std::vector> s;
+	EXPECT_TRUE(s.empty());
+}
+
+TEST(stack, ThrowsWhenTopToEmptyStack) {
+	Stack<int, std::vector> s;
+	ASSERT_ANY_THROW(s.top());
+}
+
+TEST(stack, GotTopSuccessfully) {
+	Stack<int, std::vector> s;
+	s.push(5);
+	ASSERT_EQ(s.top(), 5);
+}
+
+TEST(stack, push) {
+	Stack<int, std::vector> s;
+	s.push(1);
+	EXPECT_EQ(s.top(), 1);
+	s.push(2);
+	EXPECT_EQ(s.top(), 2);
+	s.push(3);
+	EXPECT_EQ(s.top(), 3);
+}
+
+TEST(stack, pop) {
+	Stack<int, std::vector> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.pop();
+	EXPECT_EQ(s.top(), 2);
+	EXPECT_EQ(s.size(), 2);
+}
